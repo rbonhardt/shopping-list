b7587f5670b8a630d86294711bb61bfdcb5bde8e
Ryan Bonhardt <rbonhardt@gmail.com>
2014-08-28 00:20:59 -0400

    fixed elements created disfunctioning

--- js/app.js
+++ js/app.js
@@ -10,15 +10,15 @@ $(document).ready(function() {
 
 	// complete list item
 	// if click on fa-check then remove from to-get and move to basket
-	$('.list-item').on('click', '.fa-check', function () {
+	$('.get-list').on('click', '.fa-check', function () {
 		var item = $(this).closest('.list-item').find('.item').text();
-		var listItem = $('<li class="completed-item"><i class="fa fa-check-circle"></i><span class="item">'+item+'</span></li>')
+		var listItem = $('<li class="completed-item"><i class="fa fa-check-circle"></i><span class="item">'+item+'</span></li>');
 		$('.basket-list').append(listItem);
 		$(this).closest('.list-item').remove();
 	});
 
 	// delete list item
-	$('.list-item').on('click', '.fa-close', function () {
+	$('.get-list').on('click', '.fa-close', function () {
 		$(this).closest('.list-item').remove();
 	});
 
